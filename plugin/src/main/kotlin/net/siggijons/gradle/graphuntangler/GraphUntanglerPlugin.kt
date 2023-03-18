529e8a39c1c59ae76bd5ee22eb4107589277e687
Siggi Jonsson <siggijons@siggijons.net>
2023-03-18 11:59:27 -0700

    Parse out dependencies as a list

--- plugin/src/main/kotlin/net/siggijons/gradle/graphuntangler/GraphUntanglerPlugin.kt
+++ plugin/src/main/kotlin/net/siggijons/gradle/graphuntangler/GraphUntanglerPlugin.kt
@@ -2,6 +2,7 @@ package net.siggijons.gradle.graphuntangler
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.artifacts.ProjectDependency
 
 /**
  * @suppress unused used as a plugin
@@ -18,7 +19,24 @@ class GraphUntanglerPlugin : Plugin<Project> {
         project.task("analyzeModuleGraph") { task ->
             task.doLast {
                 println("Hello from the GraphUntanglerPlugin. ${extension.message}")
+                project.rootProject.dependencyPairs(extension.configurationsToAnalyze)
+                    .forEach { (a, b, c) ->
+                        println("${a.name} -> ${b.name} ($c)")
+                    }
             }
+        }
+    }
+
+    private fun Project.dependencyPairs(
+        configurationsToAnalyze: Set<String>
+    ): List<Triple<Project, Project, String>> {
+        return subprojects.flatMap { project ->
+            project.configurations
+                .filter { configurationsToAnalyze.contains(it.name) }
+                .flatMap { configuration ->
+                    configuration.dependencies.filterIsInstance<ProjectDependency>()
+                        .map { Triple(project, it.dependencyProject, configuration.name) }
+                }
         }
     }
 }
--- plugin/src/main/kotlin/net/siggijons/gradle/graphuntangler/GraphUntanglerPluginExtension.kt
+++ plugin/src/main/kotlin/net/siggijons/gradle/graphuntangler/GraphUntanglerPluginExtension.kt
@@ -2,4 +2,6 @@ package net.siggijons.gradle.graphuntangler
 
 open class GraphUntanglerPluginExtension {
     var message: String = "Hello World!"
+
+    var configurationsToAnalyze = setOf("api", "implementation")
 }
