7ee45602c5a200c3080190bc9e912649af76ecb4
Siggi Jonsson <siggijons@siggijons.net>
2023-03-18 11:59:27 -0700

    Replicate graph calculations using only JGraphT

--- plugin/src/main/kotlin/net/siggijons/gradle/graphuntangler/GraphUntanglerPlugin.kt
+++ plugin/src/main/kotlin/net/siggijons/gradle/graphuntangler/GraphUntanglerPlugin.kt
@@ -3,6 +3,10 @@ package net.siggijons.gradle.graphuntangler
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.ProjectDependency
+import org.jgrapht.alg.scoring.BetweennessCentrality
+import org.jgrapht.graph.DefaultDirectedGraph
+import org.jgrapht.graph.DefaultEdge
+import org.jgrapht.traverse.BreadthFirstIterator
 
 /**
  * @suppress unused used as a plugin
@@ -19,12 +23,35 @@ class GraphUntanglerPlugin : Plugin<Project> {
         project.task("analyzeModuleGraph") { task ->
             task.doLast {
                 println("Hello from the GraphUntanglerPlugin. ${extension.message}")
-                project.rootProject.dependencyPairs(extension.configurationsToAnalyze)
-                    .forEach { (a, b, c) ->
-                        println("${a.name} -> ${b.name} ($c)")
-                    }
+                project.rootProject
+                    .dependencyPairs(extension.configurationsToAnalyze)
+                    .toJGraphTGraph()
+                    .nodeStatistics()
+                    .forEach(::println)
             }
         }
+    }
+
+    private fun DefaultDirectedGraph<String, DependencyEdge>.nodeStatistics(): List<NodeStatistics> {
+        val betweennessCentrality = BetweennessCentrality(this).scores
+        val roots = vertexSet().find { inDegreeOf(it) == 0 }
+        val iterator = BreadthFirstIterator(this, roots)
+        val stats = mutableListOf<NodeStatistics>()
+        while (iterator.hasNext()) {
+            val node = iterator.next()
+            val s = NodeStatistics(
+                node = node,
+                betweennessCentrality = requireNotNull(betweennessCentrality[node]) {
+                    "betweennessCentrality not found for $node"
+                },
+                degree = degreeOf(node),
+                inDegree = inDegreeOf(node),
+                outDegree = outDegreeOf(node),
+                height = iterator.getDepth(node)
+            )
+            stats.add(s)
+        }
+        return stats
     }
 
     private fun Project.dependencyPairs(
@@ -39,4 +66,29 @@ class GraphUntanglerPlugin : Plugin<Project> {
                 }
         }
     }
+
+    private fun List<Triple<Project, Project, String>>.toJGraphTGraph(): DefaultDirectedGraph<String, DependencyEdge> {
+        val g = DefaultDirectedGraph<String, DependencyEdge>(DependencyEdge::class.java)
+        forEach { (a, b, label) ->
+            g.addVertex(a.name)
+            g.addVertex(b.name)
+            g.addEdge(a.name, b.name, DependencyEdge(label = label))
+        }
+        return g
+    }
+
+    class DependencyEdge(private val label: String) : DefaultEdge() {
+        override fun toString(): String {
+            return "($source : $target : $label)"
+        }
+    }
+
+    data class NodeStatistics(
+        val node: String,
+        val betweennessCentrality: Double,
+        val degree: Int,
+        val inDegree: Int,
+        val outDegree: Int,
+        val height: Int
+    )
 }
